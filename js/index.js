6551014123d449a5631e98859b1b0c9d74a8d29c
yufong <yufong1231@gmail.com>
2018-11-28 23:35:11 +0800

    renew calender_renewEvent

--- js/index.js
+++ js/index.js
@@ -12,12 +12,21 @@ $(function() {
     },
     timeFormat: 'h:mm',
     displayEventTime: true,
+    displayEventEnd: true,
+
     events: {
       googleCalendarId: 'thu.jigaku@gmail.com',
       color: 'rgb(118,184,198)',
       textColor: 'white'
     },
+    eventRender: function(event, element, view) {
+        console.log(event.start);
+        return $('<div class="event">' + event.start.format('HH:mm') + '-' + event.end.format('HH:mm') + '</div>'
+                +'<div class="event">' + event.title + '</div>'
+                +'<p class="event">' + event.location + '</p>');
+    },
     eventClick: function(event) {
+      console.log(event);
       //window.open('https://docs.google.com/forms/d/e/1FAIpQLSf8w1_r8tKEl8Ld_tzYxSG8fKf4LP5vAAd6pFv_PEF0EwHHLQ/viewform');
       return false;
     }
